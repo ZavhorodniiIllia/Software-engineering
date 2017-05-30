146510601e485e65c4305c5b3e39fcca516f6066
Zavhorodnii Illia <golgof08@gmail.com>
2017-05-30 19:40:29 +0300

    lab2

--- lab2/lab2/Header.h
+++ lab2/lab2/Header.h
@@ -247,3 +247,199 @@ void ident() {
 		}
 	}
 }
+
+void cons_ident(string word) {
+	auto i = Identifiers.begin();
+	for (i;i != Identifiers.end(); i++) {
+		if (word == Identifiers[i->first]) {
+			syn_res << '\t' << '\t' << '\t' << '\t' << "<constant-identifier>" << endl << '\t' << '\t' << '\t' << '\t' << '\t' << word << endl;
+			break;
+		}
+		else {
+			if (i == Identifiers.end()) {
+				flag = false;
+				error = 3;
+			}
+		}
+	}
+}
+
+void proc_ident() {
+	int j; 
+	string word;
+	j = atoi(num[i_num].c_str());
+	word = num_chek(j);
+	syn_res << '\t' << "<procedure-identifiers>" << endl;
+	ident();
+}
+
+void cons() {
+	int j;
+	string word;
+	auto i = Constants.begin();
+	j = atoi(num[i_num].c_str());
+	word = num_chek(j);
+	for (i;i != Constants.end(); i++) {
+		if (word == Constants[i->first]) {
+			syn_res << '\t' << '\t' << '\t' << '\t' << "<constant>" << endl << '\t' << '\t' << '\t' << '\t' << '\t' << word << endl;
+			break;
+		}
+		else {
+			if (i == Constants.end()) {
+				flag = false;
+				error = 2;
+			}
+		}
+	}
+}
+
+void cons_decl() {
+	int j;
+	string word;
+	auto i = Constants.begin();
+	j = atoi(num[i_num].c_str());
+	i_num++;
+	word = num_chek(j);
+	for (i;i != Identifiers.end(); i++) {
+		if (word == Identifiers[i->first]) {
+			syn_res << '\t' << '\t' << '\t'  << "<constant-declaration>" << endl;
+			cons_ident(word);
+			if (flag == true) {
+				cons();
+				if (flag == true) {
+					i_num ++ ;
+					j = atoi(num[i_num].c_str);
+					word = num_chek(j);
+				}
+			}
+		}
+		else {
+			if (i == Identifiers.end()) {
+				flag = false;
+				error = 2;
+			}
+		}
+	}
+}
+
+string cons_decls() {
+	int j;
+	string word;
+	j = atoi(num[i_num].c_str());
+	i_num++;
+	word = num_chek(j);
+	if (word == "CONST") {
+		do {
+			syn_res << '\t' << '\t' << "<constant-declarations-list>" << endl << '\t' << '\t' << '\t' <<  word << endl;
+			cons_decl();
+			if (flag == true) {
+				j = atoi(num[i_num].c_str());
+				i_num++;
+				word = num_chek(j);
+			}
+		} while (word == "const"&& flag == true);
+	}
+	else {
+		flag = false;
+		error = 4;
+	}
+	return word;
+}
+
+string decl() {
+	string word;
+	syn_res << '\t' << "<declarations>" << endl;
+	word = cons_decls();
+	return word;
+}
+
+void stat_list(){
+	/*EMPTY*/
+	syn_res << '\t' << '\t' << "<statments-list>" << endl;
+}
+
+void block() {
+	int j;
+	string word;
+	j = atoi(num[i_num].c_str());
+	word = num_chek(j);
+	syn_res << "<block>" << endl;
+	word = decl();
+	if (flag == true) {
+		if (word == "BEGIN") {
+			syn_res << '\t' << word << endl;
+			stat_list();
+			j = atoi(num[i_num].c_str());
+			i_num++;
+			word = num_chek(j);
+			if (word == "END") {
+				syn_res << '\t' << word << endl << '\t' << '.' << endl;
+			}
+			else {
+				flag = false;
+				error = 6;
+			}
+		}
+		else {
+			flag = false;
+			error = 5;
+		}
+
+	}
+	
+}
+
+void program() {
+	int j;
+	string word;
+	j = atoi(num[i_num].c_str());
+	i_num++;
+	word = num_chek(j);
+	if (word == "PROGRAM") {
+		syn_res << "<program>" << endl << '\t' << word << endl;
+		proc_ident();
+		if (flag == true) { 
+			block();
+		}
+		else {
+			flag = false;
+			error = 0;
+		}
+
+	}
+	if (flag == false) {
+		syn_res << "ERROR:";
+		switch (error) {
+		case 0: {
+			syn_res << "incorrect PROGRAM" << endl;
+			break;
+			}
+		case 1: {
+			syn_res << "incorrect identyfier" << endl;
+			break;
+		}
+		case 2: {
+			syn_res << "incorrect constant" << endl;
+			break;
+		}
+		case 3: {
+			syn_res << "incorrect constant-identyfier" << endl;
+			break;
+		}
+		case 4: {
+			syn_res << "incorrect CONST" << endl;
+			break;
+		}
+		case 5: {
+			syn_res << "incorrect BEGIN" << endl;
+			break;
+		}
+		case 6: {
+			syn_res << "incorrect END" << endl;
+			break;
+		}
+		default:
+			break;
+		}
+	}
+}
